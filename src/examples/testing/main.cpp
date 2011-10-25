b5b19d0f611673e145de4c6ed09795aa0f008934
Henning Pohl <henning@still-hidden.de>
2011-10-26 01:35:03 +0200

    Update!

--- src/examples/testing/main.cpp
+++ src/examples/testing/main.cpp
@@ -1,5 +1,13 @@
 #include <oglml/vec.hpp>
 
 int main() {
+
+    const glml::vec3 v1(1.0, 2.0, 3.0);
+    const glml::vec3 v2(v1.swizzle(1, 2), 10.0);
+
+    v1.length();
+
+    v1.print();
+
     return 0;
 }
--- src/oglml/vec.hpp
+++ src/oglml/vec.hpp
@@ -2,6 +2,9 @@
 #define GLML_VEC_H
 
 #include <iostream>
+#include <functional>
+#include <vector>
+#include <cmath>
 #include <cassert>
 #include <type_traits>
 
@@ -9,7 +12,7 @@
 #define GLML_VEC_NAMESPACE_NAME vec
 
 // No template alias support in GCC yet
-#define GLML_VEC_SP template <std::size_t, typename> class
+//#define class template <std::size_t, typename> class
 
 namespace GLML_NAMESPACE_NAME {
 
@@ -32,59 +35,35 @@ namespace GLML_NAMESPACE_NAME {
     namespace GLML_VEC_NAMESPACE_NAME {
     // Forward declarations for policies
     // - Storage policies
-        template <std::size_t n, typename T>
-        class DefaultStorage;
+        struct DefaultStorage;
     }
 
     // Vec forward declarations
-    template <std::size_t n, typename T, GLML_VEC_SP SP = vec::DefaultStorage>
+    template <std::size_t n, typename T, typename SP = vec::DefaultStorage>
     union Vec;
 
     // Everything belongs to Vec
     namespace GLML_VEC_NAMESPACE_NAME {
 
         // Storage Policies
-        template <std::size_t n, typename T>
-        class DefaultStorage {
-        public:
-            // Infos
-            constexpr static bool aligned = true;
+        struct DefaultStorage {
+            template <std::size_t n, typename T>
+            class Data {
+            public:
+                inline T& operator[](std::size_t i) {
+                    assert(i < n);
+                    return mData[i];
+                }
 
-            inline T& operator[](std::size_t i) {
-                assert(i < n);
-                return mData[i];
-            }
+                inline const T& operator[](std::size_t i) const {
+                    assert(i < n);
+                    return mData[i];
+                }
 
-            inline const T& operator[](std::size_t i) const {
-                assert(i < n);
-                return mData[i];
-            }
-
-        private:
-            T mData[n];
+            private:
+                T mData[n];
+            };
         };
-
-        /*template <std::size_t n, typename T, std::size_t nHost, GLML_VEC_SP SPHost, std::size_t... Indices>
-        class UnconfiguredViewStorage {
-        public:
-            typedef Vec<nHost, T, SPHost> HostVec;
-
-            //static const
-            //static const std::size_t mIndices[nIndices];
-
-            inline T& operator[](std::size_t i) {
-                assert(i < n);
-                return *(reinterpret_cast<HostVec*>(this));
-            }
-
-            inline const T& operator[](std::size_t i) const {
-                assert(i < n);
-                return *(reinterpret_cast<HostVec*>(this));
-            }
-
-        private:
-
-        };*/
 
         // Only for View
         namespace view {
@@ -110,32 +89,62 @@ namespace GLML_NAMESPACE_NAME {
         class Private {
 
             // Private storage policies
-            template <std::size_t n, typename T>
-            class PointerStorage {
-            public:
+            struct PointerStorage {
+
                 constexpr static bool aligned = false;
 
-                inline T& operator[](std::size_t i) {
-                    assert(i < n);
-                    return *mData[i];
-                }
+                template <std::size_t n, typename T>
+                class Data {
+                public:
+                    T& operator[](std::size_t i) {
+                        assert(i < n);
+                        return *mData[i];
+                    }
 
-                inline const T& operator[](std::size_t i) const {
-                    assert(i < n);
-                    return *mData[i];
-                }
+                    const T& operator[](std::size_t i) const {
+                        assert(i < n);
+                        return *mData[i];
+                    }
 
-                inline void setPointer(std::size_t i, T* ptr) {
-                    assert(i < n);
-                    mData[i] = ptr;
-                }
+                    void setPointer(std::size_t i, T* ptr) {
+                        assert(i < n);
+                        mData[i] = ptr;
+                    }
 
-            private:
-                T* mData[n];
+                private:
+                    T* mData[n];
+                };
+            };
+
+            template <std::size_t nHost, class SPHost>
+            struct ViewStorage {
+
+               template <std::size_t n, typename T>
+               class Data {
+               public:
+                   typedef Vec<nHost, T, SPHost> HostVec;
+
+                   T& operator[](std::size_t i) {
+                       assert(i < n);
+                       return (*reinterpret_cast<HostVec*>(this))[i];
+                   }
+
+                   const T& operator[](std::size_t i) const {
+                       assert(i < n);
+                       return (*reinterpret_cast<const HostVec*>(this))[i];
+                   }
+
+                   void setIndices(const std::vector<std::size_t>& v)
+                   { mIndices = v; }
+
+               private:
+                   std::vector<std::size_t> mIndices;
+               };
+
             };
 
             // View class
-            template <std::size_t n, typename T, GLML_VEC_SP SP, std::size_t... indices>
+            template <std::size_t n, typename T, class SP, std::size_t... indices>
             class View {
             public:
                 // Assertions
@@ -151,7 +160,7 @@ namespace GLML_NAMESPACE_NAME {
 
                 // Check if indices are valid
                 constexpr static inline void check()
-                { static_assert(valid, "Indices are not valid."); }
+                { static_assert(valid, "Indices are not valid."); } // TODO
 
                 // Get indices
                 static const inline std::size_t index(std::size_t i)
@@ -183,7 +192,7 @@ namespace GLML_NAMESPACE_NAME {
         }; //Private namespace for Vec
 
         // View statics
-        template <std::size_t n, typename T, GLML_VEC_SP SP, std::size_t... indices>
+        template <std::size_t n, typename T, class SP, std::size_t... indices>
         const std::size_t Private::View<n, T, SP, indices...>::mIndices[nIndices] = { indices... };
 
     } // namespace GLML_VEC_NAMESPACE_NAME
@@ -193,28 +202,28 @@ namespace GLML_NAMESPACE_NAME {
 
         template <bool isArray, std::size_t begin>
         struct UnknownTypeAssignment {
-            template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
+            template <std::size_t nv, typename Tv, class SPv,
                       typename Tf, typename... Args>
             static inline void run(Vec<nv, Tv, SPv>& v, const Tf& f, const Args&... args);
         };
 
         template <std::size_t begin>
         struct UnknownTypeAssignment<true, begin> {
-            template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
+            template <std::size_t nv, typename Tv, class SPv,
                       typename Tf, typename... Args>
             static inline void run(Vec<nv, Tv, SPv>& v, const Tf& f, const Args&... args);
         };
 
         template <std::size_t begin>
         struct Assignment {
-            template <std::size_t nv, typename Tv, GLML_VEC_SP SPv, typename... Args>
+            template <std::size_t nv, typename Tv, class SPv, typename... Args>
             static inline void run(Vec<nv, Tv, SPv>&, const Args&...) {
                 static_assert(sizeof...(Args) == 0, "GLML corrupted.");
                 static_assert(begin == nv, "Not enough assignment parameters.");
             }
 
-            template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
-                      std::size_t nf, typename Tf, GLML_VEC_SP SPf, std::size_t... indices,
+            template <std::size_t nv, typename Tv, class SPv,
+                      std::size_t nf, typename Tf, class SPf, std::size_t... indices,
                       typename... Args>
             static inline void run(Vec<nv, Tv, SPv>& v, const vec::Private::View<nf, Tf, SPf, indices...>& f,
                                const Args&... args) {
@@ -225,8 +234,8 @@ namespace GLML_NAMESPACE_NAME {
                 Assignment<begin + step>::run(v, args...);
             }
 
-            template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
-                      std::size_t nf, typename Tf, GLML_VEC_SP SPf, typename... Args>
+            template <std::size_t nv, typename Tv, class SPv,
+                      std::size_t nf, typename Tf, class SPf, typename... Args>
             static inline void run(Vec<nv, Tv, SPv> &v, const Vec<nf, Tf, SPf>& f, const Args&... args) {
                 constexpr static std::size_t step = nf;
                 static_assert((begin + step) <= nv, "Too many assignment parameters.");
@@ -235,14 +244,14 @@ namespace GLML_NAMESPACE_NAME {
                 Assignment<begin + step>::run(v, args...);
             }
 
-            template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
+            template <std::size_t nv, typename Tv, class SPv,
                       typename Tf, typename... Args>
             static inline void run(Vec<nv, Tv, SPv>& v, const Tf& f, const Args&... args)
             { UnknownTypeAssignment<IsArray<Tf>::result, begin>::run(v, f, args...); }
         };
 
         template <bool isArray, std::size_t begin>
-        template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
+        template <std::size_t nv, typename Tv, class SPv,
                   typename Tf, typename... Args>
         inline void UnknownTypeAssignment<isArray, begin>::run
         (Vec<nv, Tv, SPv>& v, const Tf& f, const Args&... args) {
@@ -253,7 +262,7 @@ namespace GLML_NAMESPACE_NAME {
         }
 
         template <std::size_t begin>
-        template <std::size_t nv, typename Tv, GLML_VEC_SP SPv,
+        template <std::size_t nv, typename Tv, class SPv,
                   typename Tf, typename... Args>
         inline void UnknownTypeAssignment<true, begin>::run
         (Vec<nv, Tv, SPv>& v, const Tf& f, const Args&... args) {
@@ -269,9 +278,22 @@ namespace GLML_NAMESPACE_NAME {
     inline void assign(Tv& v, const First& first, const Args&... args)
     { assignm::Assignment<0>::run(v, first, args...); }
 
-    // Display
-    template <std::size_t n, typename T, GLML_VEC_SP SP>
-    inline void display(const Vec<n, T, SP>& v) {
+
+    // Length
+    template <std::size_t n, typename T, class SP>
+    inline float length(const Vec<n, T, SP>& v) {
+        static_assert(n < 4, "Length calculations are only available for vectors with " \
+                      "a dimension count of 2 or 3.");
+        float len;
+        for (std::size_t i = 0; i < n; ++i)
+            len += v[i] * v[i];
+        return std::sqrt(len);
+    }
+
+
+    // Print
+    template <std::size_t n, typename T, class SP>
+    inline void print(const Vec<n, T, SP>& v) {
         for (std::size_t i = 0; i < n; ++i) {
             std::cout << v[i];
             if (i < n - 1)
@@ -280,86 +302,120 @@ namespace GLML_NAMESPACE_NAME {
         std::cout << std::endl;
     }
 
+
     // Swizzle
     // Static swizzle
-    template <std::size_t... indices, std::size_t n, typename T, GLML_VEC_SP SP>
+    template <std::size_t... indices, std::size_t n, typename T, class SP>
     inline vec::Private::View<n, T, SP, indices...>& swizzle(Vec<n, T, SP>& v) {
         return *reinterpret_cast<vec::Private::View<n, T, SP, indices...>*>(&v);
     }
 
-    template <std::size_t... indices, std::size_t n, typename T, GLML_VEC_SP SP>
+    template <std::size_t... indices, std::size_t n, typename T, class SP>
     inline vec::Private::View<n, T, SP, indices...>& swizzle(const Vec<n, T, SP>& v) {
         return *reinterpret_cast<const vec::Private::View<n, T, SP, indices...>*>(&v);
     }
 
-
     // Runtime swizzle
-    template <std::size_t n, typename T, GLML_VEC_SP SP, typename... Indices>
+    template <std::size_t n, typename T, class SP, typename... Indices>
     inline Vec<sizeof...(Indices), T, vec::Private::PointerStorage> swizzle
     (Vec<n, T, SP>& v, Indices... indices) {
-        const std::size_t cindices[sizeof...(Indices)] = { indices... };
+        const std::vector<std::size_t> vindices = { indices... };
         Vec<sizeof...(Indices), T, vec::Private::PointerStorage> vp;
-
         for (std::size_t i = 0; i < sizeof...(Indices); ++i)
-            vp.mData.setPointer(i, &v[cindices[i]]);
-
+            vp.data.setPointer(i, &v[vindices[i]]);
         return vp;
     }
 
-
+    template <std::size_t n, typename T, class SP, typename... Indices>
+    inline const Vec<sizeof...(Indices), T, vec::Private::PointerStorage> swizzle
+    (const Vec<n, T, SP>& v, Indices... indices) {
+        std::vector<std::size_t> vindices = { indices... }; // TODO
+        Vec<sizeof...(Indices), T, vec::Private::PointerStorage> vp;
+        for (std::size_t i = 0; i < sizeof...(Indices); ++i)
+            vp.data.setPointer(i, const_cast<T*>(&v[vindices[i]]));
+        return vp;
+    }
 
     // Union Vec definition
-    template <std::size_t n, typename T, GLML_VEC_SP SP>
+    template <std::size_t n, typename T, class SP>
     union Vec {
-    public:
         // Public to all other Vecs
-        template <std::size_t, typename, GLML_VEC_SP>
+        template <std::size_t, typename, class>
         friend union Vec;
+
+        // Typedefs
+        typedef Vec<n, T, SP> ThisType;
+        typedef typename SP::template Data<n, T> ConfiguredData;
+
+        // Helper
+    private:
+        static ThisType& generate()
+        { return static_cast<ThisType*>(0); }
+
+        static const ThisType& generateConst()
+        { return static_cast<const ThisType*>(0); }
+
+        static ConfiguredData& generateData()
+        { return static_cast<ConfiguredData*>(0); }
+
+        static const ConfiguredData& generateConstData()
+        { return static_cast<const ConfiguredData*>(0); }
+
+    public:
 
         // Assertions
         static_assert(!IsArray<T>::result, "You may not store an array/pointer.");
 
-        // Typedefs
-        typedef SP<n, T> ConfiguredSP;
-
         // Friends
-        /*template <std::size_t nF, typename TF, GLML_VEC_SP SPF, typename... IndicesF>
-        friend inline Vec<sizeof...(IndicesF), TF, vec::Private::PointerStorage> glml::vec::swizzle
-        (Vec<nF, TF, SPF>& v, IndicesF... indices);*/
 
         // Ctor, dtor
         Vec()
         { }
 
+        Vec(const Vec& rhs) : data(rhs.data)
+        { }
+
         template <typename First, typename... Args>
         Vec(const First& first, const Args&... args)
-        { assign(first, args...); }
+        { try { assign(first, args...); } catch(...) { std::abort(); } }
+
+        ~Vec() { }
 
         // Index operator
-        T& operator[](std::size_t i)
-        { return mData[i]; }
-
         const T& operator[](std::size_t i) const
-        { return mData[i]; }
+        { return data[i]; }
+
+        T& operator[](std::size_t i) // TODO
+        { return data[i]; }
 
         // Additional methods
         template <typename First, typename... Args>
-        void assign(const First& first, const Args&... args)
+        auto assign(const First& first, const Args&... args) ->
+        decltype(glml::assign(ThisType::generate(), first, args...))
         { glml::assign(*this, first, args...); }
 
-        void display() const
-        { glml::display(*this); }
+        decltype(glml::length(generate())) length() const
+        { return glml::length(*this); }
+
+        decltype(glml::print(ThisType::generate())) print() const
+        { glml::print(*this); }
 
         template <std::size_t... indices>
-        vec::Private::View<n, T, SP, indices...>& swizzle()
+        decltype(glml::swizzle<indices...>(ThisType::generate())) swizzle()
         { return glml::swizzle<indices...>(*this); }
 
         template <std::size_t... indices>
-        vec::Private::View<n, T, SP, indices...>& swizzle() const
+        const Vec<sizeof...(indices), T, vec::Private::PointerStorage> swizzle() const
         { return glml::swizzle<indices...>(*this); }
 
         template <typename... Indices>
-        Vec<sizeof...(Indices), T, vec::Private::PointerStorage> swizzle(Indices... indices)
+        auto swizzle(Indices... indices) ->
+        decltype(glml::swizzle(ThisType::generate(), indices...))
+        { return glml::swizzle(*this, indices...); }
+
+        template <typename... Indices>
+        auto swizzle(Indices... indices) const ->
+        decltype(glml::swizzle(ThisType::generate(), indices...))
         { return glml::swizzle(*this, indices...); }
 
         // GLSL Compatibility
@@ -377,8 +433,13 @@ namespace GLML_NAMESPACE_NAME {
         vec::Private::View<n, T, SP, 0, 1, 2> xyz;
 
         // The data
-        ConfiguredSP mData;
+        ConfiguredData data;
     };
+
+    // Typedefs
+    typedef Vec<2, float> vec2;
+    typedef Vec<3, float> vec3;
+    typedef Vec<4, float> vec4;
 
 } // namespace GLML_NAMESPACE_VEC
 
